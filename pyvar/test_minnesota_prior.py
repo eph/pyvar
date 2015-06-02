7656fdb1cc13653e368e9c0af2d256240c9a00ca
Ed Herbst <ed.herbst@gmail.com>
2015-06-02 11:01:34 -0400

    add test for minnesota prior

---
+++ pyvar/test_minnesota_prior.py
@@ -0,0 +1,53 @@
+from __future__ import division
+
+import numpy as np
+import pandas as p
+
+from numpy.testing import assert_equal, assert_almost_equal
+
+from unittest import TestCase
+
+from pyvar import MinnesotaPrior
+
+
+class TestMinnesota(TestCase):
+
+    def test_minnesota_prior(self):
+        
+        datfile = '/mq/home/m1eph00/projects/var-smc/var_smc_working/lib_var/sz_2008_joe_data.csv'
+
+        yy = p.read_csv(datfile, names=['XGAP', 'INF', 'FFR'])
+        lam = np.ones((6,))
+        premom = np.c_[yy.mean().values, yy.std().values].T
+
+        minnpr = MinnesotaPrior(yy, lam, p=1, presample_moments=premom)
+
+
+        # From MDN/FS code
+        YYdum = np.array([[ 0.024863787734143,                  0,                  0], 
+                          [                 0,  0.025530860432406,                  0],  
+                          [                 0,                  0,  0.033133670737398],  
+                          [ 0.024863787734143,                  0,                  0],  
+                          [                 0,  0.025530860432406,                  0],  
+                          [                 0,                  0,  0.033133670737398],  
+                          [-0.004767798723404,  0.037277177127660,  0.060582978723404],  
+                          [-0.004767798723404,                  0,                  0],  
+                          [                 0,  0.037277177127660,                  0],  
+                          [                 0,                  0,  0.060582978723404]]) 
+
+
+        XXdum = np.array([[   0.024863787734143,                  0,                  0,        0],
+                          [                   0,  0.025530860432406,                  0,        0],  
+                          [                   0,                  0,  0.033133670737398,        0], 
+                          [                   0,                  0,                  0,        0],  
+                          [                   0,                  0,                  0,        0],  
+                          [                   0,                  0,                  0,        0],  
+                          [  -0.004767798723404,  0.037277177127660,  0.060582978723404,   1.0000],  
+                          [  -0.004767798723404,                  0,                  0,        0],  
+                          [                   0,  0.037277177127660,                  0,        0],  
+                          [                   0,                  0,  0.060582978723404,        0]])
+
+
+        yydum_minn, xxdum_minn = minnpr.get_pseudo_obs()
+        assert_almost_equal(yydum_minn, YYdum)
+        assert_almost_equal(xxdum_minn, XXdum)
