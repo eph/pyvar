0bd9b63c04f26925e736eec6e56d7082f4a2c8d7
Ed Herbst <ed.herbst@gmail.com>
2015-08-25 09:56:19 -0400

    more tests for Minnesota Prior

--- pyvar/test_minnesota_prior.py
+++ pyvar/test_minnesota_prior.py
@@ -12,7 +12,7 @@ from pyvar import MinnesotaPrior
 
 class TestMinnesota(TestCase):
 
-    def test_minnesota_prior(self):
+    def test_minnesota_prior_frank(self):
         
         datfile = '/mq/home/m1eph00/projects/var-smc/var_smc_working/lib_var/sz_2008_joe_data.csv'
 
@@ -83,13 +83,71 @@ class TestMinnesota(TestCase):
         assert_almost_equal(yydum_minn, YYdum)
         assert_almost_equal(xxdum_minn, XXdum)
 
-
+        # matlab call
+        # [ydu, xdu, breaks] = varprior_h(3, 5, 1, [1 1 1 1 4]', [ybar' sbar'])
         YYdum = np.loadtxt('ydu_p5.txt')
         XXdum = np.loadtxt('xdu_p5.txt')
         lam = np.ones((6,))
         lam[3] = 4.0
         minnpr = MinnesotaPrior(yy, lam, p=5, presample_moments=premom)
         yydum_minn, xxdum_minn = minnpr.get_pseudo_obs()
-        print np.c_[YYdum, yydum_minn]
+
         assert_almost_equal(yydum_minn, YYdum)
         assert_almost_equal(xxdum_minn, XXdum)
+
+    def test_minnesota_prior_mark(self):
+        
+        datfile = '/mq/home/m1eph00/projects/var-smc/var_smc_working/lib_var/sz_2008_joe_data.csv'
+        yy = p.read_csv(datfile, names=['XGAP', 'INF', 'FFR'])
+
+        lam = [2.5, 1, 1, 1, 3, 1]
+        ybar = [-0.0010, 0.0122, 0.0343]
+        sbar = [ 0.0076, 0.0111, 0.0092]
+        premom = np.array([ybar, sbar])
+        m = MinnesotaPrior(yy, lam, p=5, presample_moments=premom, lamxx=True)
+
+        # matlab call
+        # prihyp = pri_var_dummy([2.5, 1, 1, 3, 1, 1,  1], premom, 5, 3, 5*3+1)
+        from scipy.io import loadmat
+        Omega = loadmat('/mq/home/m1eph00/python-repo/var/pyvar/prihyp.mat')['prihyp'][0][0][0]
+        Phi_star = loadmat('/mq/home/m1eph00/python-repo/var/pyvar/prihyp.mat')['prihyp'][0][0][1]
+        Psi = loadmat('/mq/home/m1eph00/python-repo/var/pyvar/prihyp.mat')['prihyp'][0][0][2]
+        nu = loadmat('/mq/home/m1eph00/python-repo/var/pyvar/prihyp.mat')['prihyp'][0][0][3]
+
+        assert_almost_equal(Omega, m.Omega)
+        assert_almost_equal(Phi_star, m.Phi_star)
+        assert_almost_equal(Psi, m.Psi)
+        
+
+    def test_minnesota_prior_fortran(self):
+        datfile = '/mq/home/m1eph00/projects/var-smc/var_smc_working/lib_var/sz_2008_joe_data.csv'
+        yy = p.read_csv(datfile, names=['XGAP', 'INF', 'FFR'])
+
+        lam = [2.5, 1, 1, 1, 3, 1]
+        ybar = [-0.0010, 0.0122, 0.0343]
+        sbar = [ 0.0076, 0.0111, 0.0092]
+        premom = np.array([ybar, sbar])
+        m = MinnesotaPrior(yy, lam, p=5, presample_moments=premom, lamxx=True)
+
+        # fortran call
+        # call write_prior_hyper(2.5_wp,1.0_wp,1.0_wp,1.0_wp,3.0_wp,1.0_wp,1.0_wp, &
+        #  (/-0.0010_wp, 0.0122_wp, 0.0343_wp/), (/ 0.0076_wp, 0.0111_wp, 0.0092_wp/))
+        Omega_inv = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_Omega_inv.txt')
+        Phi_star = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_phistar.txt')
+        Psi = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_iw_Psi.txt')
+        nu = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_iw_nu.txt')
+
+        assert_almost_equal(Omega_inv, np.linalg.inv(m.Omega))
+        assert_almost_equal(Phi_star, m.Phi_star.flatten(order='F'))
+        assert_almost_equal(Psi, m.Psi)
+
+        lam = [2.5, 2, 1, 1.3, 3, 1]
+        m = MinnesotaPrior(yy, lam, p=5, presample_moments=premom, lamxx=True)
+        Omega_inv = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_Omega_inv_two.txt')
+        Phi_star = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_phistar_two.txt')
+        Psi = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_iw_Psi_two.txt')
+        nu = np.loadtxt('/mq/home/m1eph00/python-repo/var/pyvar/hyper_iw_nu_two.txt')
+
+        assert_almost_equal(Omega_inv, np.linalg.inv(m.Omega))
+        assert_almost_equal(Phi_star, m.Phi_star.flatten(order='F'))
+        assert_almost_equal(Psi, m.Psi)
