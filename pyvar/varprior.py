460c3993a645c68d1c1d4ed1c1ab2569b7c0f7c1
Ed Herbst <ed.herbst@gmail.com>
2016-09-13 10:44:09 -0400

    update varprior to fix ny bug

--- pyvar/varprior.py
+++ pyvar/varprior.py
@@ -64,7 +64,9 @@ class DummyVarPrior(Prior):
         return Phi, Sigma
 
     def get_pseudo_obs(self):
-        return None, None    
+        return None, None
+
+    
 class DiffusePrior(DummyVarPrior):
 
     def __init__(self):
@@ -90,11 +92,11 @@ class SimsZhaSVARPrior(Prior):
         mu5 = hyperpara[5]
         mu6 = hyperpara[6]
         
-        mu0 = 1/lam0
-        mu1 = 1/lam1
-        mu2 = 1/lam2
+        mu0 = 1.0/lam0
+        mu1 = 1.0/lam1
+        mu2 = 1.0/lam2
         mu3 = -lam3
-        mu4 = 1/lam4
+        mu4 = 1.0/lam4
 
         ypre = np.array(ypre)
         T, ny = ypre.shape
@@ -557,83 +559,6 @@ class TwoExternalInstrumentsSVARPrior(SimsZhaSVARPrior):
         return sz + self.gamma.logpdf(x[-2]) + pdfx
 
 
-class ExternalInstrumentsSVARPrior(SimsZhaSVARPrior):
-
-    def __init__(self, *args, **kwargs):
-
-        self.gamma = kwargs.pop('gamma', None)
-        self.rho = kwargs.pop('rho', None)
-        self.signu = kwargs.pop('signu', None)
-
-        self.parameterization = 'rho'
-        if self.signu is not None:
-            self.parameterization = 'signu'
-            
-        super(ExternalInstrumentsSVARPrior, self).__init__(*args, **kwargs)
-
-        self.ny = self.ny+1
-        
-    def rvs(self, size=1, flatten=True):
-        x = super(ExternalInstrumentsSVARPrior, self).rvs(size=size, flatten=flatten)
-        gamma = self.gamma.rvs(size=size)
-        if self.parameterization == 'rho':
-            rho = self.rho.rvs(size=size)
-        else:
-            rho = self.signu.rvs(size=size)
-        x0 = np.c_[x, gamma, rho]
-        
-        if flatten == True:
-            return x0 
-
-    def para_trans(self, *args, **kwargs):
-        dtype=kwargs.pop('dtype', float)
-        sqrt = np.sqrt
-        if dtype==object:
-            import sympy
-            sqrt = sympy.sqrt
-        if len(args) == 1:
-            A0, Aplus = super(ExternalInstrumentsSVARPrior, self).para_trans(args[0][:-2], dtype=dtype) 
-
-            gam = args[0][-2]
-            if self.parameterization == 'signu':
-                signu = args[0][-1]
-                rho = gam**2 / (gam**2 + signu**2)
-            else:
-                rho = args[0][-1]
-
-            A0t = np.zeros((self.ny, self.ny), dtype=dtype)
-            A0t[:-1, :][:, :-1] = A0
-            A0t[:-1, -1] = -A0[:, 0] / sqrt( (1-rho)/rho)
-            A0t[-1, -1] = 1. / (sqrt( (1-rho)/rho)*gam)
-
-            Aplust = np.zeros((self.ny*self.p + self.cons, self.ny), dtype=dtype)
-            for i in range(self.p):
-                ind0 = i*self.n 
-                ind0t = i*self.ny
-                Aplust[ind0t:ind0t+self.n, :][:, :-1] = Aplus[ind0:ind0+self.n, :]
-
-            if self.cons == True:
-                Aplust[-1, :-1] = Aplus[-1, :]
-
-            Aplust[:, -1] = -Aplust[:, 0] / sqrt( (1-rho)/rho)
-            return A0t, Aplust
-
-        elif len(args) == 2:
-            return args[0], args[1]
-    
-            
-
-    def logpdf(self, x):
-        A0, Aplus = super(ExternalInstrumentsSVARPrior, self).para_trans(x[:-2]) 
-        sz = super(ExternalInstrumentsSVARPrior, self).logpdf(A0, Aplus)
-
-        if self.parameterization == 'rho':
-            pdfx = self.rho.logpdf(x[-1])
-        else:
-            pdfx = self.signu.logpdf(x[-1])
-            
-        return sz + self.gamma.logpdf(x[-2]) + pdfx
-
 
 class FixedEndogeneitySVARPrior(SimsZhaSVARPrior):
     def __init__(self, *args, **kwargs):
@@ -724,6 +649,7 @@ class FixedEndogeneitySVARPrior(SimsZhaSVARPrior):
 
         elif len(args) == 2:
             return args[0], args[1]
+
 
 
 import sympy
@@ -937,7 +863,8 @@ class MinnesotaPrior(DummyVarPrior):
         self.n = ny;
         self.p = p;
         self.cons = cons;
-
+        self.ny = ny;
+        
         n = ny
         max_n = ny*(ny+1)/2
         x = range(int(max_n))
